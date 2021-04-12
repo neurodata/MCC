2d3e66954816e910de0964c38f3807696d668000
v715 <vgopala4@jhu.edu>
2021-04-12 08:30:20 -0400

    Save simulation run metadata

--- supplement/S2-vertex-simulation/vertex_simulation.py
+++ supplement/S2-vertex-simulation/vertex_simulation.py
@@ -104,16 +104,30 @@ def experiment(sample_size, block_1, block_2, p, effect_size, generate_func, rep
     m_per_pop = sample_size // 2
     block_size = np.array([block_1, block_2])
     n_nodes = block_1 + block_2
+    labels = np.array([0] * block_1 + [1] * block_2)
 
     def worker(i):
         X, Y = generate_func(p, effect_size, block_size, m_per_pop)
         pvalues_omni = pd.DataFrame.from_dict(test_omni(X, Y, n_nodes))
         pvalues_mdmr = pd.DataFrame.from_dict(test_mdmr(X, Y, n_nodes))
         pvalues_nx = pd.DataFrame.from_dict(test_nxstats(X, Y, n_nodes))
-        df = pd.concat([pvalues_omni, pvalues_mdmr, pvalues_nx])
+        df = make_df(
+            pvalues_omni, pvalues_mdmr, pvalues_nx, labels, block_2, generate_func
+        )
         df.to_csv(f"results/tmp/{block_2}-{generate_func.__name__}-{i}.csv")
 
     Parallel(-1)(delayed(worker)(i) for i in range(reps))
+
+
+def make_df(pvalues_omni, pvalues_mdmr, pvalues_nx, labels, block_2, generate_func):
+    df = pd.concat([pvalues_omni, pvalues_mdmr, pvalues_nx])
+    df = df.T
+    df.columns = ["omni", "mdmr", "stats"]
+    df["node"] = df.index
+    df["label"] = labels
+    df["n_signal_vertices"] = int(block_2)
+    df["generating_func"] = generate_func.__name__
+    return df
 
 
 # %% Simulation parameters
